409042ace0ac3dacc79b4c34260f5447f28c1fd9
weiweihook <weiweihook@163.com>
2024-09-21 16:44:38 +0800

    Initial commit

---
+++ src/vvi/vvi_v1/config/case1/modify.py
@@ -0,0 +1,27 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import math
+from scipy.interpolate import interp1d, make_interp_spline
+import pandas as pd
+import seaborn as sns
+import matplotlib.colors as mcolors
+import os
+
+def delete_defore(filename,output):
+    with open(filename) as f, open(output, 'w') as temp:
+        for line in f:
+            parts = line.split('=')
+            if len(parts) > 1:
+                temp.write(parts[1])
+            else:
+                temp.write(line)
+    with open(output, 'r') as f:
+        content = f.read()
+        new_content = content.replace('p','')
+    with open(output, 'w') as f:
+        f.write(new_content)
+
+
+if __name__ == "__main__":
+    delete_defore('chip_param_dcap.txt', 'init_chip_param.txt')
---
+++ src/vvi/vvi_v1/demo.py
@@ -0,0 +1,14 @@
+#-- annealing method demo
+
+import numpy as np
+from scipy.optimize import dual_annealing
+
+func = lambda x: np.sum(x*x - 10*np.cos(2*np.pi*x)) + 10*np.size(x)
+
+lw = [-5.12] * 10
+
+up = [5.12] * 10
+
+sol = dual_annealing(func, bounds=list(zip(lw, up)))
+
+print(sol)
